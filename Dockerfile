2bf694cb870247b83bbebd196093bd1b5db2385f
son <thswjddn0812@gmail.com>
2025-12-30 23:48:51 +0900

    docker 버전 수정

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # Base image
-FROM node:18-slim
+# Base image
+FROM node:20-slim
 
 # Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
 # Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,37 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { corsConfig } from './configs/cors.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(corsConfig);
-  // --- 스웨거 설정 시작 ---
-  const config = new DocumentBuilder()
-    .setTitle('JikguMate API')
-    .setDescription('직구메이트 팀 프로젝트를 위한 API 문서입니다.')
-    .setVersion('1.0')
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token',
-    )
-    .build();
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors(corsConfig);
+    // --- 스웨거 설정 시작 ---
+    const config = new DocumentBuilder()
+      .setTitle('JikguMate API')
+      .setDescription('직구메이트 팀 프로젝트를 위한 API 문서입니다.')
+      .setVersion('1.0')
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'access-token',
+      )
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // 'api'는 접속 주소가 됩니다. (localhost:3000/api)
-  // --- 스웨거 설정 끝 ---
-  app.use(cookieParser());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  await app.listen(process.env.PORT || 3000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    // --- 스웨거 설정 끝 ---
+    app.use(cookieParser());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('SERVER STARTUP ERROR:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
